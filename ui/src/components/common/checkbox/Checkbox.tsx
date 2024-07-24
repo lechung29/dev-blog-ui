14e709411121757d77405ee210fc0c50da5d9006
lequocchung29 <chungle924@gmail.com>
2024-07-25 01:51:43 +0700

    support policy checkbox error and disable button when have error

--- ui/src/components/common/checkbox/Checkbox.tsx
+++ ui/src/components/common/checkbox/Checkbox.tsx
@@ -1,16 +1,29 @@
-import { Checkbox, FormControlLabel, FormControlLabelProps } from "@mui/material";
-import React from "react";
-
+import { Checkbox, FormControlLabel, FormControlLabelProps, FormHelperText } from "@mui/material";
+import React, { Fragment, useMemo } from "react";
+import "./index.scss";
 export interface ICheckboxProps extends FormControlLabelProps {
     labelStyle?: React.CSSProperties;
+    error?: boolean;
+    errorMessage?: string;
 }
 const CheckboxView: React.FunctionComponent<ICheckboxProps> = (props) => {
-    const { labelStyle, ...rest } = props;
-    return <FormControlLabel 
-        {...rest} 
-        
-        control={<Checkbox />} 
-    />;
+    const { labelStyle, error, errorMessage, ...rest } = props;
+    const onRenderMessage: JSX.Element = useMemo(() => {
+        return (error || errorMessage)
+            ? <FormHelperText 
+                error={error}
+                className={`g-helpText-message ${!!errorMessage && "g-helpText-error-message"}`}
+            >
+                {errorMessage || ""}
+            </FormHelperText>
+            : <Fragment></Fragment>
+    }, [errorMessage, error])    
+    return (
+        <div className={`g-checkbox-container`}>
+            <FormControlLabel {...rest} control={<Checkbox />} />
+            {onRenderMessage}
+        </div>
+    );
 };
 
 export { CheckboxView as Checkbox };
