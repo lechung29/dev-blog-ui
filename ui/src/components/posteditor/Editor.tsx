74fcb17990dd1a33668c47570fb5fe53bf3c6117
lequocchung29 <chungle924@gmail.com>
2024-08-07 22:32:49 +0700

    Update create post editor

---
+++ ui/src/components/posteditor/Editor.tsx
@@ -0,0 +1,80 @@
+import { useCallback, useRef, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { htmlToMarkdown, markdownToHtml } from "../../pages/dashboard/Parse";
+import uploadToCloudinary from "../../services/helpers/upload";
+import "./index.scss";
+
+export interface EditorContentChanged {
+	html: string;
+	markdown: string;
+}
+
+export interface EditorProps {
+	value?: string;
+	onChange?: (changes: EditorContentChanged) => void;
+}
+
+export default function Editor(props: EditorProps) {
+	const [value, setValue] = useState<string>(markdownToHtml(props.value || ""));
+	const reactQuillRef = useRef<ReactQuill>(null);
+
+	const onChange = (content: string) => {
+		setValue(content);
+
+		if (props.onChange) {
+			props.onChange({
+				html: content,
+				markdown: htmlToMarkdown(content),
+			});
+		}
+	};
+
+	const imageHandler = useCallback(() => {
+		const input = document.createElement("input");
+		input.setAttribute("type", "file");
+		input.setAttribute("accept", "image/*");
+		input.click();
+		input.onchange = async () => {
+			if (input !== null && input.files !== null) {
+				const file = input.files[0];
+				const url = await uploadToCloudinary(file);
+				const quill = reactQuillRef.current;
+				if (quill) {
+					const range = quill.getEditorSelection();
+					range && quill.getEditor().insertEmbed(range.index, "image", url);
+				}
+			}
+		};
+	}, []);
+
+	return (
+		<ReactQuill
+			ref={reactQuillRef}
+			theme="snow"
+			placeholder="Nhập nội dung..."
+			modules={{
+				toolbar: {
+					container: [
+						[{ header: "1" }, { header: "2" }, { font: [] }],
+						[{ size: [] }],
+						["bold", "italic", "underline", "strike", "blockquote"],
+						[{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
+						["link", "image"],
+						["code-block"],
+						["clean"],
+					],
+					handlers: {
+						image: imageHandler,
+					},
+				},
+				clipboard: {
+					matchVisual: false,
+				},
+			}}
+			formats={["header", "font", "size", "bold", "italic", "underline", "strike", "blockquote", "list", "bullet", "indent", "link", "image", "code-block"]}
+			value={value}
+			onChange={onChange}
+		/>
+	);
+}
---
+++ ui/src/services/helpers/upload.ts
@@ -0,0 +1,20 @@
+const uploadToCloudinary = async (file: File): Promise<string> => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append(
+      "upload_preset",
+      process.env.REACT_APP_CLOUDINARY_PRESET_NAME as string
+    );
+    const res = await fetch(
+      `https://api.cloudinary.com/v1_1/${
+        process.env.REACT_APP_CLOUDINARY_NAME as string
+      }/upload`,
+      { method: "POST", body: formData }
+    );
+    const data = await res.json();
+    const url = data.url;
+  
+    return url
+}
+  
+export default uploadToCloudinary
