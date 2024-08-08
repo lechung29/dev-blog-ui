979e2553db1068c3b898f55715b4fda2a7b56be9
lequocchung29 <chungle924@gmail.com>
2024-08-08 22:45:31 +0700

    Create data table component

--- ui/src/pages/dashboard/postmanagement/user/UserPostManagement.tsx
+++ ui/src/pages/dashboard/postmanagement/user/UserPostManagement.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import DashboardLayout from '../../../../layout/DashboardLayout'
+import React, { useRef } from "react";
+import DashboardLayout from "../../../../layout/DashboardLayout";
+import DataTable, { IDataTabelRef } from "../../../../components/datatable/DataTable";
+import { postManagementColumn } from "../../../../components/datatable/utils";
+import { useImmerState } from "../../../../hook/useImmerState";
+import { PostService } from "../../../../services/posts/PostService";
 
-const UserPostManagement = () => {
-  return (
-    <DashboardLayout>
-        <div className='g-dashboard-content-section'>
-        Hêlo
-        </div>
-    </DashboardLayout>
-  )
+interface IPostManagementState {
+	items: any[];
 }
 
-export default UserPostManagement
+const initialState: IPostManagementState = {
+	items: [],
+};
+
+const UserPostManagement = () => {
+	const [state, setState] = useImmerState<IPostManagementState>(initialState);
+	const dataTableRef = useRef<IDataTabelRef>(null);
+	const getData = () => {
+		return PostService.getPosts({ limit: 10 }).then((data) => {
+			const rowItems = data.data.map((item) => ({ ...item, id: item._id, author: item.author.displayName }));
+			setState({ items: rowItems });
+		});
+	};
+
+	const handleChangeSelection = (selection) => {
+		console.log("Selection changed:", selection);
+		// Perform your action here
+	};
+
+  const handleReload = () => {
+    dataTableRef.current?.reload()
+  }
+
+	return (
+		<DashboardLayout>
+			<div className="g-dashboard-content-section">
+        <button onClick={handleReload}>Reload</button>
+        <DataTable onSelection={handleChangeSelection} ref={dataTableRef} columns={postManagementColumn} items={state.items} getData={getData} />
+			</div>
+		</DashboardLayout>
+	);
+};
+
+export default UserPostManagement;
