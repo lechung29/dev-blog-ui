f21b4e8b58696b9ebc96412c1004f8afedc66679
Killian.Le <killian.le@avepoint.com>
2024-09-24 01:23:17 +0700

    Update app file

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,38 +2,38 @@ import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useAppSelector } from "./redux/store/store";
 import { userState } from "./redux/reducers/users/UserSlice";
-// import Home from "./pages/home/Home";
-// import Login from "./pages/auth/Login";
-// import { SignUp } from "./pages/auth/Register";
-// import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home";
+import Login from "./pages/auth/Login";
+import { SignUp } from "./pages/auth/Register";
+import Profile from "./pages/profile/Profile";
 import CommonRoute from "./components/PrivateRoute/CommonRoute";
 import AdminRoute from "./components/PrivateRoute/AdminRoute";
 import UserRoute from "./components/PrivateRoute/UserRoute";
-// import CreatePost from "./pages/dashboard/createpost/CreatePost";
-// import UserPostManagement from "./pages/dashboard/postmanagement/user/UserPostManagement";
-// import FavouritePost from "./pages/dashboard/favouritepost/FavouritePost";
-// import Overview from "./pages/dashboard/overview/Overview";
-// import SearchPage from "./pages/search/SearchPage";
-// import PostPage from "./pages/post/PostPage";
-// import UserManagement from "./pages/dashboard/usermanagement/UserManagement";
+import CreatePost from "./pages/dashboard/createpost/CreatePost";
+import UserPostManagement from "./pages/dashboard/postmanagement/user/UserPostManagement";
+import FavouritePost from "./pages/dashboard/favouritepost/FavouritePost";
+import Overview from "./pages/dashboard/overview/Overview";
+import SearchPage from "./pages/search/SearchPage";
+import PostPage from "./pages/post/PostPage";
+import UserManagement from "./pages/dashboard/usermanagement/UserManagement";
 
-// import NotFound from "./pages/pageNotFound/PageNotFound";
-// import EditPost from "./pages/dashboard/editpost/EditPost";
+import NotFound from "./pages/pageNotFound/PageNotFound";
+import EditPost from "./pages/dashboard/editpost/EditPost";
 
 
-const NotFound = React.lazy(() => import("./pages/pageNotFound/PageNotFound"))
-const EditPost = React.lazy(() => import("./pages/dashboard/editpost/EditPost"))
-const Home = React.lazy(() => import("./pages/home/Home"));
-const Login = React.lazy(() => import("./pages/auth/Login"));
-const SignUp = React.lazy(() => import("./pages/auth/Register").then(module => ({ default: module.SignUp })));
-const Profile = React.lazy(() => import("./pages/profile/Profile"));
-const CreatePost = React.lazy(() => import("./pages/dashboard/createpost/CreatePost"));
-const UserPostManagement = React.lazy(() => import("./pages/dashboard/postmanagement/user/UserPostManagement"));
-const FavouritePost = React.lazy(() => import("./pages/dashboard/favouritepost/FavouritePost"));
-const Overview = React.lazy(() => import("./pages/dashboard/overview/Overview"));
-const SearchPage = React.lazy(() => import("./pages/search/SearchPage"));
-const PostPage = React.lazy(() => import("./pages/post/PostPage"));
-const UserManagement = React.lazy(() => import("./pages/dashboard/usermanagement/UserManagement"));
+// const NotFound = React.lazy(() => import("./pages/pageNotFound/PageNotFound"))
+// const EditPost = React.lazy(() => import("./pages/dashboard/editpost/EditPost"))
+// const Home = React.lazy(() => import("./pages/home/Home"));
+// const Login = React.lazy(() => import("./pages/auth/Login"));
+// const SignUp = React.lazy(() => import("./pages/auth/Register").then(module => ({ default: module.SignUp })));
+// const Profile = React.lazy(() => import("./pages/profile/Profile"));
+// const CreatePost = React.lazy(() => import("./pages/dashboard/createpost/CreatePost"));
+// const UserPostManagement = React.lazy(() => import("./pages/dashboard/postmanagement/user/UserPostManagement"));
+// const FavouritePost = React.lazy(() => import("./pages/dashboard/favouritepost/FavouritePost"));
+// const Overview = React.lazy(() => import("./pages/dashboard/overview/Overview"));
+// const SearchPage = React.lazy(() => import("./pages/search/SearchPage"));
+// const PostPage = React.lazy(() => import("./pages/post/PostPage"));
+// const UserManagement = React.lazy(() => import("./pages/dashboard/usermanagement/UserManagement"));
 
 function App() {
 	const { user } = useAppSelector(userState)
